4af71d06756d43e0c9b72d298f3bd9341cbc1723
Alexandre Garske Machado <alexandre.machado@compasso.com.br>
2023-10-24 15:08:02 -0300

    Add spring configuration

---
+++ src/test/java/com/alexandre/java/backend/productapi/ProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandre.java.backend.productapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
